8b1f6e9fda820271c348651274eb89424b57d779
kevinKS94 <kevinschild@hotmail.com>
2020-09-10 17:41:49 -0300

    Entrega individual HTML y CSS

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: '' });
+});
+
+router.get('/registro', function(req, res, next) {
+  res.render('registro', { title: '' });
+});
+
+module.exports = router;
